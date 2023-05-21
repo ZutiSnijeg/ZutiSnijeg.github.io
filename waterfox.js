8c24d88ae2368b60ff09b07f0c4aa9a914e39aa2
ZutiSnijeg <108091006+ZutiSnijeg@users.noreply.github.com>
2023-05-21 21:29:53 +0200

    waterfox.js
    quick search

--- waterfox.js
+++ waterfox.js
@@ -1,3 +1,4 @@
+sites = ["https://www.youtube.com/", "https://web.whatsapp.com/", "https://www.twitch.tv/", "https://www.chess.com/home", "https://github.com/"];
 function search(){
     let searchText = document.getElementById("searchInput").value;
     if(document.getElementById("searchInput").value != ""){
@@ -8,9 +9,17 @@ function search(){
 
 
 document.getElementById("searchInput").addEventListener("keydown", e => {
+    //convert asci to normal number
+    converted = e.keyCode - 49
     if (e.keyCode === 13) {
         search();
     }
+    else if (0 <= converted && converted < sites.length && document.getElementById("searchInput").value == "")
+    {
+        window.open(sites[converted], "_self");
+    }
+    console.log(converted);
+
 });
 
 document.getElementById("searchInput").value = "";
